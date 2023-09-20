fb271910b0194e1276b4e80a611d676235395893
Patrick Leary <paleary@davidson.edu>
2023-09-20 12:36:07 -0400

    Changed a bit of the JS log

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,7 @@
 /* javascript */
 
 console.log("Figma link: https://www.figma.com/file/GoYWsXRk0c3LKCFLkgpx6j/Unbranding?type=design&node-id=18-152&mode=design&t=gLo6BY9wpj78im3u-0")
-console.log("This project shows that even though Marlboro (and other cigarettes) are becoming much less popular, there are still new types of nicotine products that are coming out that are just as popular and arguably more dangerous.")
+console.log("This project shows that even though Marlboro (and other cigarettes)")
+console.log("are becoming much less popular, there are still new types of nicotine products")
+console.log("that are coming out that are just as popular and arguably more dangerous.")
+console.log("Nicotine is just being rebranded and sold to a younger audience as a new product.")
