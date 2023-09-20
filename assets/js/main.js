367aa541b37ea0cc3e5a5a5c83f64897e384da3a
Patrick Leary <paleary@davidson.edu>
2023-09-20 00:42:52 -0400

    Finished most of site (still need to do stuff with fonts)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,5 @@
 
 /* javascript */
+
+console.log("Figma link: https://www.figma.com/file/GoYWsXRk0c3LKCFLkgpx6j/Unbranding?type=design&node-id=18-152&mode=design&t=gLo6BY9wpj78im3u-0")
+console.log("This project shows that even though Marlboro (and other cigarettes) are becoming much less popular, there are still new types of nicotine products that are coming out that are just as popular and arguably more dangerous.")
